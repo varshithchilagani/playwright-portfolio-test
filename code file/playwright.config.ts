d208d5090cb33adceb73d3fa2cb807e007e0d383
Varshith Chilagani <varshithchilagani03@gmail.com>
2026-01-02 21:52:46 +0530

    Delete code file directory

--- code file/playwright.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from '@playwright/test';
-
-export default defineConfig({
-  use: {
-    baseURL: 'https://varshith-chilagani-portfolio.netlify.app/',
-    video: 'on',
-  },
-  testDir: 'tests',
-});
